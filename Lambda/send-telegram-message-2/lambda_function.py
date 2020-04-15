6be4cc0b65f56ead76e3ac1b8582f6048a78ee49
Evelyn <clumsycherry@users.noreply.github.com>
2020-04-15 19:25:50 -0400

    Update lambda_function.py

--- Lambda/send-telegram-message-2/lambda_function.py
+++ Lambda/send-telegram-message-2/lambda_function.py
@@ -2,33 +2,32 @@ import json
 import os
 from botocore.vendored import requests
 
-def executeCommand(command:str, chat_id:int, patient_id:int, medication_id:int)->str:
+def executeCommand(command:str, chat_id:int, text:str)->str:
     hyper_url = "http://ec544.hopto.org:7786/"
     if command == "patients":
         requests.post(hyper_url + "patients", data={"did": chat_id}).json()
-        dummy = 1;
+        #requests.post("https://1eb82cd0e02229fbc16e5923449d0dcf.m.pipedream.net", data={"did": chat_id}).json()
     elif command == "prescriptions":
-        requests.post(hyper_url + "medlist", data={"did": chat_id, "pid": patient_id}).json()
-        dummy = 1;
+        #requests.post(hyper_url + "medlist", data={"did": chat_id, "pid": text.replace('/prescriptions', '').strip()}).json()
+        requests.post("https://1eb82cd0e02229fbc16e5923449d0dcf.m.pipedream.net", data={"did": chat_id, "pid": text.replace('/prescriptions', '').strip()}).json()
     elif command == "progress":
-        requests.post(hyper_url + "progress", data={"did": chat_id, "mid": medication_id}).json()
-        dummy = 1;
+        #requests.post(hyper_url + "progress", data={"did": chat_id, "pid": text.replace('/progress', '').strip()}).json()
+        requests.post("https://1eb82cd0e02229fbc16e5923449d0dcf.m.pipedream.net", data={"did": chat_id, "pid": text.replace('/progress', '').strip()}).json()
     elif command == "meddetail":
-        requests.post(hyper_url + "meddetail", data={"did": chat_id}).json()
-        dummy = 1;
+        #requests.post(hyper_url + "meddetail", data={"did": chat_id, "pid": text.replace('/meddetail', '').strip()}).json()
+        requests.post("https://1eb82cd0e02229fbc16e5923449d0dcf.m.pipedream.net", data={"did": chat_id, "pid": text.replace('/meddetail', '').strip()}).json()
     else:
         return "Valid bot commands:\n" \
                 "/patients - name & age of patients\n" \
                 "/progress - side effects or symptoms this week\n" \
                 "/prescriptions [patient ID] - list of medications\n" \
-                "/meddetail [med ID] - details of 1 medication"
+                "/meddetail [med ID] - details of 1 medication {}".format(command)
     return 'ok'
 
 def lambda_handler(event, context):
+
     field_errors = {}
     command = ""
-    patient_id = 23;
-    medication_id = 0;
     
     if 'message' in event:
         message = event['message']
@@ -39,7 +38,7 @@ def lambda_handler(event, context):
             field_errors['chat_id'] = "Dr's ID missing"
         
         if 'text' in message:    
-            text = message['text'].strip()
+            text = message['text']
         else:
             field_errors['text'] = "Text missing"
             
@@ -48,7 +47,7 @@ def lambda_handler(event, context):
             for entity in entities:
                 if entity['type'] == "bot_command":
                     command = text[entity['offset']+1:entity['offset']+entity['length']]
-                    telegram_msg = executeCommand(command, chat_id, patient_id, medication_id)
+                    telegram_msg = executeCommand(command, chat_id, text)
                     if telegram_msg != 'ok':
                         params = {'chat_id': chat_id, 'text': telegram_msg}
                         telegram_token = os.environ['TELEGRAM_BOT_TOKEN']
